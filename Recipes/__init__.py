2a0c3e6e906cd8fef0f961daa08571b6a741fe8a
Ethan Senator <ethansenator@gmail.com>
2014-11-18 21:10:10 -0500

    Added Recipe model
    Also moved config.py to base and deleted and replaced app.db

--- Recipes/__init__.py
+++ Recipes/__init__.py
@@ -8,10 +8,9 @@ from flask.ext.sqlalchemy import SQLAlchemy
 from flask.ext.login import LoginManager
 
 app = Flask(__name__)
-app.config.from_pyfile('config.py')
-
-
+app.config.from_object('config')
 db = SQLAlchemy(app)
+
 lm = LoginManager()
 lm.init_app(app)
 lm.login_view = 'login'
